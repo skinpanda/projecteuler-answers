479f4fea58001cfb1e12cdf2265c2643ee9748e2
skinpanda <33093093+skinpanda@users.noreply.github.com>
2017-11-29 11:16:34 +0800

    Create problem_6.py

---
+++ solutions/problem_6.py
@@ -0,0 +1,7 @@
+# Title: Sum square difference
+
+# Use list comprehension
+sumofsquares = sum([x**2 for x in range(1, 101)])
+squareofsums = sum(range(1, 101))**2
+
+print(squareofsums - sumofsquares)
